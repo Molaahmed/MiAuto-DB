3cd38d5a2072f779e9934cc3ae9d592ff50d1dd7
Redzhep Molaahmed <redjep.molaahmed.rm@gmial.com>
2021-09-28 14:26:39 +0200

    DB-8|Created a simple User seeder

--- MiAuto-DB/database/seeders/UsersTableSeeder.php
+++ MiAuto-DB/database/seeders/UsersTableSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,5 +17,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
