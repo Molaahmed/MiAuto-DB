48ffa683cfdf225a2fa9fe7b089c94427cc5e909
Redzhep Molaahmed <redjep.molaahmed.rm@gmial.com>
2021-09-28 15:06:45 +0200

    DB-8| Created a users controller
    Implementing a simple login that verifies you and return a token.

---
+++ MiAuto-DB/app/Http/Controllers/UsersController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    //
+
+    function logIn(Request $request){
+
+        $user = User::where('email', $request->email)->first();
+        
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response([
+                'message' => ['These credentials do not match out records.'] 
+            ],404); 
+        }
+
+        $token = $user->createToken('my-app-token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' =>$token
+        ];
+        
+        return response($response, 201);
+    }
+}
