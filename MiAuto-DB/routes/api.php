761bbf84ec6fce947a9b038415b2dcc2b62efc4e
Redzhep Molaahmed <33726739+Molaahmed@users.noreply.github.com>
2021-11-02 10:21:53 +0100

    Revert "Db 47 create cars as admin"

--- MiAuto-DB/routes/api.php
+++ MiAuto-DB/routes/api.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TokensController;
-use App\Http\Controllers\RegistrationController;
-use App\Http\Controllers\UsersController;
-use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,19 +16,4 @@ use App\Http\Controllers\AdminController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-    
 });
-
-
-
-Route::post('/registerClient', [RegistrationController::class, 'storeClient']);
-Route::post('/registerGarage', [RegistrationController::class, 'storeGarage']);
-
-Route::post('/login', [TokenController::class, 'store']);
-Route::get('/logout', [TokenController::class, 'destroy']);
-
-Route::put('/users/update', [UserController::class, 'updateProfile']);
-
-
-//REMINDER: this endpoint needs to have an admin authorization
-Route::post('/cars/create', [AdminController::class,'createCar']);
