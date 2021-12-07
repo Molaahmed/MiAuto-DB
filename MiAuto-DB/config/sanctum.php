833154bd11e81a908121fbfd58ec5b92cd27fe40
Riad Joul <joulriad@gmail.com>
2021-12-07 14:53:36 +0100

    Authentication with Token API

--- MiAuto-DB/config/sanctum.php
+++ MiAuto-DB/config/sanctum.php
@@ -15,7 +15,11 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
+<<<<<<< HEAD
         'localhost',
+=======
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1, mi-auto-db-jbp5o.ondigitalocean.app',
+>>>>>>> b36cb20fa58766ce4ff4d1e62038845a88769929
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
